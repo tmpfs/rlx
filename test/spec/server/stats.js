a1ba284d208397c677a5aa700190a348547a16ac
cyberfunk <hello@cyberfunk.org>
2014-08-16 12:47:16 +0800

    Assert on stats command.

---
+++ test/spec/server/stats.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var fs = require('fs');
+var expect = require('chai').expect;
+var config = require('../../util/config');
+var pkg = config.paths.pkg;
+var program = config.program;
+
+describe('rlx:', function() {
+  this.timeout(5000);
+  before(function(done) {
+    done();
+  })
+  it('should retrieve server statistics', function(done){
+    var mock = config.file('server-stats.json');
+    var args = [
+      'stats',
+      '--no-color',
+      '-s', config.server.default,
+      '-o', mock
+    ];
+    var def = program(require(pkg), config.name)
+    def.program.on('complete', function(req) {
+      var doc = config.json(mock);
+      var keys = Object.keys(doc);
+      expect(doc).to.be.an('object');
+      expect(keys).to.be.an('array');
+      expect(keys.length).to.be.gt(0);
+      done();
+    })
+    def.parse(args);
+  });
+})
