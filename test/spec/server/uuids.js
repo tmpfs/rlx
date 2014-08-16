6d1fcca576753a29fb535665434725f5db7ec83d
cyberfunk <hello@cyberfunk.org>
2014-08-16 12:49:48 +0800

    Assert on uuids command.

---
+++ test/spec/server/uuids.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var fs = require('fs');
+var expect = require('chai').expect;
+var config = require('../../util/config');
+var pkg = config.paths.pkg;
+var program = config.program;
+
+describe('rlx:', function() {
+  this.timeout(5000);
+  before(function(done) {
+    done();
+  })
+  it('should retrieve uuids', function(done){
+    var mock = config.file('server-uuids.json');
+    var args = [
+      'uuids',
+      '--no-color',
+      '-s', config.server.default,
+      '-o', mock
+    ];
+    var def = program(require(pkg), config.name)
+    def.program.on('complete', function(req) {
+      var doc = config.json(mock);
+      expect(doc).to.be.an('object');
+      expect(doc.uuids).to.be.an('array').of.length(1);
+      expect(doc.uuids[0]).to.be.a('string');
+      done();
+    })
+    def.parse(args);
+  });
+})
