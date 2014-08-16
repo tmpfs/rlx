49a93e101fcf30f215d8885dd1c56bbd61cc20a6
cyberfunk <hello@cyberfunk.org>
2014-08-16 10:55:59 +0800

    Assert on offset and bytes options.

--- test/spec/server/log.js
+++ test/spec/server/log.js
@@ -20,8 +20,26 @@ describe('rlx:', function() {
     var def = program(require(pkg), config.name)
     def.program.on('complete', function(req) {
       var doc = config.text(mock);
-      console.dir(doc);
-      //expect(doc).to.be.an('string');
+      expect(doc).to.be.a('string');
+      done();
+    })
+    def.parse(args);
+  });
+  it('should retrieve log data using offset and bytes', function(done){
+    var offset = 128;
+    var bytes = 128;
+    var mock = config.file('server-log.txt');
+    var args = [
+      'log', '--no-color',
+      '-s', config.server.default,
+      '-o', mock,
+      '--offset=' + offset,
+      '--bytes=' + bytes
+    ];
+    var def = program(require(pkg), config.name)
+    def.program.on('complete', function(req) {
+      var doc = config.text(mock);
+      expect(doc).to.be.a('string').of.length(bytes);
       done();
     })
     def.parse(args);
