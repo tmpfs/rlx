5cea0a56c5f311b81084a1d238841ad7d7c9b3f3
cyberfunk <hello@cyberfunk.org>
2014-08-17 21:31:10 +0800

    Assert on template get with file extension.

--- test/spec/template/get.js
+++ test/spec/template/get.js
@@ -5,6 +5,15 @@ var config = require('../../util/config');
 var pkg = config.paths.pkg;
 var program = config.program;
 
+function assert(doc) {
+  expect(doc).to.be.an('object');
+  expect(doc._id).to.be.a('string');
+  expect(doc.name).to.be.a('string');
+  expect(doc.password).to.be.a('string');
+  expect(doc.roles).to.be.a('string');
+  expect(doc.type).to.be.a('string');
+}
+
 describe('rlx:', function() {
   this.timeout(5000);
   it('should get template', function(done){
@@ -20,12 +29,25 @@ describe('rlx:', function() {
     var def = program(require(pkg), config.name)
     def.program.on('complete', function(req) {
       var doc = config.json(mock);
-      expect(doc).to.be.an('object');
-      expect(doc._id).to.be.a('string');
-      expect(doc.name).to.be.a('string');
-      expect(doc.password).to.be.a('string');
-      expect(doc.roles).to.be.a('string');
-      expect(doc.type).to.be.a('string');
+      assert(doc);
+      done();
+    })
+    def.parse(args);
+  });
+  it('should get template with file extension', function(done){
+    var mock = config.file('template-get-extension.json');
+    var args = [
+      'tpl',
+      'get',
+      '-t',
+      'user/new.json',
+      '--no-color',
+      '-o', mock
+    ];
+    var def = program(require(pkg), config.name)
+    def.program.on('complete', function(req) {
+      var doc = config.json(mock);
+      assert(doc);
       done();
     })
     def.parse(args);
