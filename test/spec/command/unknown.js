71ec1edf65bb92aed8926da42b3cb56c04a5c52f
cyberfunk <hello@cyberfunk.org>
2014-08-19 17:44:51 +0800

    Assert on unknown command.

---
+++ test/spec/command/unknown.js
@@ -0,0 +1,24 @@
+var expect = require('chai').expect;
+var config = require('../../util/config');
+var pkg = config.paths.pkg;
+var program = config.program;
+var database = config.database.default;
+
+describe('rlx:', function() {
+  this.timeout(5000);
+  it('should error on unknown command', function(done){
+    var args = [
+      'unknown',
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      var def = errors.EUNKNOWN_COMMAND;
+      expect(err).to.be.instanceof(Error);
+      expect(err.key).to.eql(def.key);
+      done();
+    })
+    def.parse(args);
+  });
+})
