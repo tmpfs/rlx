482a1f3bf5e16c2d6fea9daf183c2458aac77d4a
cyberfunk <hello@cyberfunk.org>
2014-08-19 23:39:43 +0800

    Assert on doc/* database required.

--- test/spec/doc/error.js
+++ test/spec/doc/error.js
@@ -34,4 +34,60 @@ describe('rlx:', function() {
     })
     def.parse(args);
   });
+  it('should error on doc/get (database required)', function(done){
+    var args = [
+      'doc',
+      'get',
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.db(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
+  it('should error on doc/ls (database required)', function(done){
+    var args = [
+      'doc',
+      'ls',
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.db(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
+  it('should error on doc/rev (database required)', function(done){
+    var args = [
+      'doc',
+      'rev',
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.db(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
+  it('should error on doc/rm (database required)', function(done){
+    var args = [
+      'doc',
+      'rm',
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.db(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
 })
