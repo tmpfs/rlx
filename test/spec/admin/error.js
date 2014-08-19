5cc27821faf9a28956308b23e86555f17ef47f82
cyberfunk <hello@cyberfunk.org>
2014-08-19 22:26:48 +0800

    Assert on admin argument validation.

--- test/spec/admin/error.js
+++ test/spec/admin/error.js
@@ -3,6 +3,22 @@ var config = require('../../util/config');
 var pkg = config.paths.pkg;
 var program = config.program;
 
+
+var assert = {};
+assert.username = function(err, errors) {
+  var def = errors.EUSERNAME_REQUIRED;
+  expect(def).to.be.an('object');
+  expect(err).to.be.instanceof(Error);
+  expect(err.key).to.eql(def.key);
+}
+
+assert.password = function(err, errors) {
+  var def = errors.EPASSWORD_REQUIRED;
+  expect(def).to.be.an('object');
+  expect(err).to.be.instanceof(Error);
+  expect(err.key).to.eql(def.key);
+}
+
 describe('rlx:', function() {
   this.timeout(5000);
   it('should error on admin/add (missing username)', function(done){
@@ -14,10 +30,7 @@ describe('rlx:', function() {
     var def = program(require(pkg), config.name);
     var errors = def.program.errors;
     def.program.on('error', function(err) {
-      var def = errors.EUSERNAME_REQUIRED;
-      expect(def).to.be.an('object');
-      expect(err).to.be.instanceof(Error);
-      expect(err.key).to.eql(def.key);
+      assert.username(err, errors);
       done();
     })
     def.parse(args);
@@ -32,10 +45,35 @@ describe('rlx:', function() {
     var def = program(require(pkg), config.name);
     var errors = def.program.errors;
     def.program.on('error', function(err) {
-      var def = errors.EPASSWORD_REQUIRED;
-      expect(def).to.be.an('object');
-      expect(err).to.be.instanceof(Error);
-      expect(err.key).to.eql(def.key);
+      assert.password(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
+  it('should error on admin/get (missing username)', function(done){
+    var args = [
+      'admin',
+      'get',
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.username(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
+  it('should error on admin/rm (missing username)', function(done){
+    var args = [
+      'admin',
+      'rm',
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.username(err, errors);
       done();
     })
     def.parse(args);
