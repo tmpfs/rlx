70d37c29033f7359c1427b237fb24f5301c8673d
cyberfunk <hello@cyberfunk.org>
2014-08-19 23:43:23 +0800

    Assert on doc/* id required.
    Also assert on ENO_DB_FILE for doc/ls.

--- test/spec/doc/error.js
+++ test/spec/doc/error.js
@@ -18,6 +18,14 @@ assert.db = function(err, errors) {
   expect(err.key).to.eql(def.key);
 }
 
+assert.dbfile = function(err, errors) {
+  var def = errors.ENO_DB_FILE;
+  expect(def).to.be.an('object');
+  expect(err).to.be.instanceof(Error);
+  expect(err.key).to.eql(def.key);
+}
+
+
 describe('rlx:', function() {
   this.timeout(5000);
   it('should error on doc/add (database required)', function(done){
@@ -86,6 +94,82 @@ describe('rlx:', function() {
     var errors = def.program.errors;
     def.program.on('error', function(err) {
       assert.db(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
+
+  it('should error on doc/add (id required)', function(done){
+    var args = [
+      'doc',
+      'add',
+      '-d=' + config.database.default,
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.id(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
+  it('should error on doc/get (id required)', function(done){
+    var args = [
+      'doc',
+      'get',
+      '-d=' + config.database.default,
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.id(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
+  it('should error on doc/ls (no db file)', function(done){
+    var args = [
+      'doc',
+      'ls',
+      '-d=' + config.database.default,
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.dbfile(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
+  it('should error on doc/rev (id required)', function(done){
+    var args = [
+      'doc',
+      'rev',
+      '-d=' + config.database.default,
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.id(err, errors);
+      done();
+    })
+    def.parse(args);
+  });
+  it('should error on doc/rm (id required)', function(done){
+    var args = [
+      'doc',
+      'rm',
+      '-d=' + config.database.default,
+      '--no-color'
+    ];
+    var def = program(require(pkg), config.name);
+    var errors = def.program.errors;
+    def.program.on('error', function(err) {
+      assert.id(err, errors);
       done();
     })
     def.parse(args);
