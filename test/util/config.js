1cf2b856a705d084c3850d6432b56a935f5ca8c1
cyberfunk <hello@cyberfunk.org>
2014-08-24 17:21:01 +0800

    Segregate mock data.
    So that assertion helpers may depend upon mock data without a circular
    require dependency.

--- test/util/config.js
+++ test/util/config.js
@@ -1,64 +1,6 @@
 var fs = require('fs');
 var path = require('path');
-var cdb = require('cdb');
-var base = path.normalize(path.join(__dirname, '..', '..'));
-var target = path.join(base, 'target')
-var program = require('../../lib/rlx');
-var userdb = require('../../lib/command/user/userdb');
-
-var config = {
-  name: 'rlx-test-runner',
-  program: program,
-  editor: path.join(base, 'test', 'bin', 'editor'),
-  cdb: cdb,
-  server: {
-    default: process.env.rlx_test_server || 'http://localhost:5984'
-  },
-  conf: {
-    section: 'mock-config-section',
-    key: 'mock-config-key',
-    value: 'mock-config-value'
-  },
-  database: {
-    default: 'mock/database',
-    users: userdb.default,
-    unknown: 'mock-unknown-database'
-  },
-  user: {
-    name: 'mock-user',
-    pass: 'secret',
-    id: userdb.prefix + 'mock-user',
-    roles: 'admin,user',
-    type: 'user'
-  },
-  admin: {
-    name: 'mock-admin',
-    pass: 'secret',
-    alt: {
-      name: 'mock-alt-admin',
-      pass: 'secret'
-    }
-  },
-  design: {
-    auth: '_auth'
-  },
-  document: {
-    id: 'mock-document',
-    bool: false,
-    int: 1024,
-    float: 1.67,
-    arr: [1,2,3],
-    str: 'value',
-    nil: null
-  },
-  paths: {
-    base: base,
-    pkg: path.join(base, 'package.json'),
-    target: target,
-    fixtures: path.join(base, 'test', 'fixtures'),
-  },
-  fixtures: {}
-}
+var config = require('./mock');
 
 var editor, visual;
 config.edit = {};
@@ -76,7 +18,7 @@ config.edit.restore = function(cb) {
 }
 
 config.file = function(name, content) {
-  var file = path.join(target, name);
+  var file = path.join(config.paths.target, name);
   if(content) fs.writeFileSync(file, content);
   return file;
 }
---
+++ test/util/mock.js
@@ -0,0 +1,62 @@
+var path = require('path');
+var cdb = require('cdb');
+var base = path.normalize(path.join(__dirname, '..', '..'));
+var target = path.join(base, 'target')
+var program = require('../../lib/rlx');
+var userdb = require('../../lib/command/user/userdb');
+
+var mock = {
+  name: 'rlx-test-runner',
+  program: program,
+  editor: path.join(base, 'test', 'bin', 'editor'),
+  cdb: cdb,
+  server: {
+    default: process.env.rlx_test_server || 'http://localhost:5984'
+  },
+  conf: {
+    section: 'mock-config-section',
+    key: 'mock-config-key',
+    value: 'mock-config-value'
+  },
+  database: {
+    default: 'mock/database',
+    users: userdb.default,
+    unknown: 'mock-unknown-database'
+  },
+  user: {
+    name: 'mock-user',
+    pass: 'secret',
+    id: userdb.prefix + 'mock-user',
+    roles: 'admin,user',
+    type: 'user'
+  },
+  admin: {
+    name: 'mock-admin',
+    pass: 'secret',
+    alt: {
+      name: 'mock-alt-admin',
+      pass: 'secret'
+    }
+  },
+  design: {
+    auth: '_auth'
+  },
+  document: {
+    id: 'mock-document',
+    bool: false,
+    int: 1024,
+    float: 1.67,
+    arr: [1,2,3],
+    str: 'value',
+    nil: null
+  },
+  paths: {
+    base: base,
+    pkg: path.join(base, 'package.json'),
+    target: target,
+    fixtures: path.join(base, 'test', 'fixtures'),
+  },
+  fixtures: {}
+}
+
+module.exports = mock;
