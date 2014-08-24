3504d60ee543f94fa89ccd6c43c8c372fb448410
cyberfunk <hello@cyberfunk.org>
2014-08-24 18:25:37 +0800

    Move db list test.

--- test/spec/db/lifecycle.js
+++ test/spec/db/lifecycle.js
@@ -8,6 +8,23 @@ var assert = config.assert.db;
 
 describe('rlx:', function() {
   this.timeout(5000);
+  it('should retrieve database list', function(done){
+    var mock = config.file('database-ls.json');
+    var args = [
+      'db',
+      'ls',
+      '--no-color',
+      '-s=' + config.server.default,
+      '-o', mock
+    ];
+    var def = program(require(pkg), config.name)
+    def.program.on('complete', function(req) {
+      var doc = config.json(mock);
+      assert.list(doc);
+      done();
+    })
+    def.parse(args);
+  });
   it('should create database', function(done){
     var mock = config.file('database-add.json');
     var args = [
