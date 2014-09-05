dd830479720846427660847308acd544dd7ec756
cyberfunk <hello@cyberfunk.org>
2014-09-05 12:18:26 +0800

    Rename previous to before in level/set.

--- test/assert/level.js
+++ test/assert/level.js
@@ -3,9 +3,8 @@ var expect = require('chai').expect;
 function set(doc, level) {
   expect(doc).to.be.an('object');
   expect(doc.level).to.be.a('string').to.eql(level);
-  expect(doc.previous).to.be.a('string');
+  expect(doc.before).to.be.a('string');
 }
-
 
 function get(doc) {
   expect(doc).to.be.an('object');
