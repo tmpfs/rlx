45afa7ee093eb0b748df434d95b42856c39cf954
cyberfunk <hello@cyberfunk.org>
2014-09-30 21:04:58 +0800

    Disable compiler in interactive.

--- test/spec/app/lifecycle.js
+++ test/spec/app/lifecycle.js
@@ -156,7 +156,6 @@ describe('rlx:', function() {
   it('should query design document view', function(done){
     var mock = config.file('app-view.json');
     var args = qt.getArguments('app/view', {output: mock});
-    console.dir(args);
     var def = program(require(pkg), config.name)
     def.program.on('complete', function(req) {
       var doc = config.json(mock);
